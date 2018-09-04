3382ff1e45733e2a903f4051441d3f7a05211e9c
Glitch (tundra-sled) <none>
2018-09-04 16:52:29 +0000

    Added template and css

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 import json
 from collections import OrderedDict
@@ -18,10 +18,16 @@ def date_response(dt):
   mimetype='application/json'
   )
 
-@app.route("/api/timestamp/", defaults={'date_string': None})
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/api/timestamp/")
 @app.route("/api/timestamp/<date_string>/")
-def date(date_string):
+def date(date_string=None):
+  # If data string is not None
   if date_string:
+    # Timestamps
     try:
       stamp = int(float((date_string)))
       d = datetime(1970, 1, 1, 0, 0, 0, tzinfo=gmt) +\
@@ -29,13 +35,12 @@ def date(date_string):
       d = d.replace(tzinfo=gmt)
       return date_response(d)
     except ValueError as e:
-      print(e)
+      # Date strings compliant with ISO-8601
       try:
         d = parser.parse(date_string)
         d = d.replace(tzinfo=gmt)
         return date_response(d)
       except ValueError as e:
-        print(e)
         return Response(
             json.dumps({"error": "Invalid Date"}),
             mimetype='application/json'
